ae57c53718741983996441d9fd6c3f2d25fc5eca
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2024-09-27 10:33:08 -0500

    Add in-memory and Eclipse account repositories
    Introduced `InMemoryAccountRepository` and `EclipseAccountAdapter` implementations for `AccountRepository`. Added unit tests for `EclipseAccountAdapter` and initial setup for `TaskCreatorTest`. Updated project dependencies and configurations, including Spring Boot Actuator and virtual threads.

--- src/main/java/dev/nathanlively/adapter/out/eclipse/EclipseAccountAdapter.java
+++ src/main/java/dev/nathanlively/adapter/out/eclipse/EclipseAccountAdapter.java
@@ -3,15 +3,19 @@ package dev.nathanlively.adapter.out.eclipse;
 import dev.nathanlively.application.port.AccountRepository;
 import dev.nathanlively.domain.Account;
 import dev.nathanlively.domain.DataRoot;
+import org.eclipse.serializer.persistence.types.Storer;
 import org.eclipse.store.integrations.spring.boot.types.concurrent.Read;
 import org.eclipse.store.integrations.spring.boot.types.concurrent.Write;
 import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EclipseAccountAdapter implements AccountRepository {
     private final EmbeddedStorageManager storageManager;
+    private static final Logger log = LoggerFactory.getLogger(EclipseAccountAdapter.class);
 
     public EclipseAccountAdapter(EmbeddedStorageManager storageManager) {
         this.storageManager = storageManager;
@@ -20,6 +24,7 @@ public class EclipseAccountAdapter implements AccountRepository {
 
     private void initializeRoot() {
         if (storageManager.root() == null) {
+            log.info("Initializing root.");
             storageManager.setRoot(new DataRoot());
             storageManager.storeRoot();
         }
@@ -30,7 +35,8 @@ public class EclipseAccountAdapter implements AccountRepository {
     public void save(Account account) {
         DataRoot root = (DataRoot) storageManager.root();
         root.accounts().add(account);
-        storageManager.store(root.accounts());
+//        storageManager.store(root.accounts());
+        saveWithEagerStoring(root);
     }
 
     @Read
@@ -45,5 +51,11 @@ public class EclipseAccountAdapter implements AccountRepository {
     public List<Account> findAll() {
         DataRoot root = (DataRoot) storageManager.root();
         return new ArrayList<>(root.accounts().all());
+    }
+
+    private void saveWithEagerStoring(DataRoot root) {
+        Storer eagerStorer = storageManager.createEagerStorer();
+        eagerStorer.store(root);
+        eagerStorer.commit();
     }
 }
---
+++ src/test/java/dev/nathanlively/application/TaskCreatorTest.java
@@ -0,0 +1,5 @@
+package dev.nathanlively.application;
+
+class TaskCreatorTest {
+
+}
